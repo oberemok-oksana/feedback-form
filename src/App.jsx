b4356fc7b7f0ac1ac2c5a6385cfbbe2ff1a0f8ea
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 23:05:37 +0700

    add GlobalStyles to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
+import GlobalStyles from "./components/styled/Global";
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Main />
       <Footer />
     </>
