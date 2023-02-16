9848451259277fce4ff4a857d00f7057e954401a
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 20:50:54 +0700

    implement StyledInput styled component

---
+++ src/components/styled/StyledInput.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledInput = styled.input`
+  padding: 30px 45px;
+  border-color: #dcdcdc;
+  border-radius: 10px;
+  font-family: "Apercu Arabic Pro";
+  font-size: 18px;
+  color: #2d2d2d;
+`;
+
+export default StyledInput;
