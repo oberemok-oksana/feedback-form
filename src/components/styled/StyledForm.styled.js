ec5a6275dcf6e922810d8769ba30f3fd0eabe495
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 20:53:05 +0700

    implement StyledForm styled component

---
+++ src/components/styled/StyledForm.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 563px;
+  gap: 8px;
+  z-index: 5;
+  position: relative;
+`;
+
+export default StyledForm;
