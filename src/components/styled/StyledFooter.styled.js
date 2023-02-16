3e6e765248b5762b772f5856996d869dc227f7e6
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 20:53:14 +0700

    implement StyledFooter styled component

---
+++ src/components/styled/StyledFooter.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  background-color: #fafafa;
+  border-top-color: #d8d8d8;
+  height: 180px;
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  bottom: 0;
+`;
+
+export default StyledFooter;
