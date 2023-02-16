80ebe3ff0f0973541eb81851c6f52417163ecbc6
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 19:00:40 +0700

    add img with images for bg

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,42 @@ function App() {
   return (
     <>
       <main className="container">
+        <img
+          src="../public/images/red_cartoon.svg"
+          alt="yellow smile"
+          className="yellow-smile-1"
+        />
+        <img
+          src="../public/images/cloud-big.svg"
+          alt="cloud"
+          className="big-cloud-1"
+        />
+        <img
+          src="../public/images/cloud-big.svg"
+          alt="cloud"
+          className="big-cloud-2"
+        />
+        <img
+          src="../public/images/cloud-small.svg"
+          alt="cloud"
+          className="small-cloud-1"
+        />
+        <img
+          src="../public/images/cloud-big.svg"
+          alt="cloud"
+          className="big-cloud-3"
+        />
+        <img src="../public/images/Mask.svg" alt="globe" className="globe" />
+        <img
+          src="../public/images/red_cartoon3.svg"
+          alt="red smile"
+          className="big-red-smile"
+        />
+        <img
+          src="../public/images/goodie1.svg"
+          alt="yellow smile"
+          className="yellow-smile-2"
+        />
         <h1 className="title">Reach out to us</h1>
         <form className="form">
           <input
@@ -23,7 +59,7 @@ function App() {
             name="user-text"
             id="user-text"
             cols="30"
-            rows="10"
+            rows="6"
             placeholder="Your message*"
           ></textarea>
           <button type="submit" className="form__btn">
@@ -32,6 +68,21 @@ function App() {
         </form>
       </main>
       <footer className="footer">
+        <img
+          src="../public/images/red_cartoon3.svg"
+          alt="red smile"
+          className="footer__big-red-smile"
+        />
+        <img
+          src="../public/images/red_cartoon.svg"
+          alt="yellow smile"
+          className="footer___yellow-smile"
+        />
+        <img
+          src="../public/images/green.svg"
+          alt="green smile"
+          className="footer__green-smile"
+        />
         <ul className="social-media-list">
           <li>
             <a href="https://www.instagram.com/">
