5804af0ed8b19b8f01478c69a719ff933ec04761
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 20:53:25 +0700

    implement GlobeImg styled component

---
+++ src/components/styled/GlobeImg.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const GlobeImg = styled.img`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  /* width: 100%; */
+`;
+
+export default GlobeImg;
