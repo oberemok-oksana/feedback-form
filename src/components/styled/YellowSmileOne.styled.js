3d47417adcd0d6913c9244f126fb3844a86ec044
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 20:49:57 +0700

    implement YellowSmileOne and Two styled components

---
+++ src/components/styled/YellowSmileOne.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const YellowSmileOne = styled.img`
+  position: absolute;
+  left: 2.01%;
+  right: 91.85%;
+  top: 6.55%;
+  bottom: 85.84%;
+  transform: rotate(-35.19deg);
+`;
+
+export default YellowSmileOne;
---
+++ src/components/styled/YellowSmileTwo.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const YellowSmileTwo = styled.img`
+  position: absolute;
+  top: 73%;
+  right: 36%;
+  z-index: 3;
+`;
+
+export default YellowSmileTwo;
