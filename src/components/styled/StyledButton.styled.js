213d2a725d819a80c76417df9e9f9a11645c1894
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 20:50:24 +0700

    implement StyledButton styled component

---
+++ src/components/styled/StyledButton.styled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: #fad34f;
+  cursor: pointer;
+  color: #ffffff;
+  padding: 25px 0;
+  border-radius: 500px;
+  max-width: 218px;
+  font-family: "Apercu Arabic Pro";
+  font-size: 18px;
+  margin-top: 23px;
+  border: none;
+
+  &:hover {
+    color: #222222;
+    transition: all 0.5s ease;
+  }
+`;
+export default StyledButton;
