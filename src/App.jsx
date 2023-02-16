f183885659112d74e06f495fa3e5f715ee18bf5e
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 16:11:52 +0700

    add main with title and form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <main className="main-section">
+        <h1 className="title">Reach out to us</h1>
+        <form className="form">
+          <input
+            type="text"
+            placeholder="Your name*"
+            name="user-name"
+            className="form__input"
+          />
+          <input
+            type="email"
+            placeholder="Your e-mail*"
+            name="user-email"
+            className="form__input"
+          />
+          <textarea
+            className="form__textarea"
+            name="user-text"
+            id="user-text"
+            cols="30"
+            rows="10"
+            placeholder="Your message*"
+          ></textarea>
+          <button type="submit" className="form__btn">
+            Send message
+          </button>
+        </form>
+      </main>
+    </>
+  );
 }
 
 export default App;
