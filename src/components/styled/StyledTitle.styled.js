6c1883717ba01cee2ac75e139719cc4fdfa3385b
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 20:51:32 +0700

    implement StyledTitle styled component

---
+++ src/components/styled/StyledTitle.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledTitle = styled.h1`
+  font-family: "Apercu Arabic Pro";
+  color: #3e3e3e;
+  font-size: 40px;
+`;
+
+export default StyledTitle;
