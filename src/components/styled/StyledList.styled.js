605c89cd9f5983a99e043958fab0576b423a5c68
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-16 20:51:15 +0700

    implement StyledList styled component

---
+++ src/components/styled/StyledList.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledList = styled.ul`
+  padding-left: 25%;
+  padding-top: 79px;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin: 0;
+  gap: 25px;
+`;
+
+export default StyledList;
