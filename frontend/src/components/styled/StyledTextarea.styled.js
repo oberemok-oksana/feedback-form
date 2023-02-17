00fdb86afdbf92b05c77472c3e4fb8b2c70c9d0a
Oksana Oberemok <Pylypenkok@gmail.com>
2023-02-17 19:54:32 +0700

    add style for error

--- frontend/src/components/styled/StyledTextarea.styled.js
+++ frontend/src/components/styled/StyledTextarea.styled.js
@@ -1,8 +1,8 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledTextarea = styled.textarea`
   padding: 30px 45px;
-  border-color: #dcdcdc;
+  border: 2px solid #dcdcdc;
   border-radius: 10px;
   font-family: "Apercu Arabic Pro";
   font-size: 18px;
@@ -10,7 +10,17 @@ const StyledTextarea = styled.textarea`
 
   @media (max-width: 600px) {
     padding: 10px 25px;
-  } ;
+  }
+
+  ${(props) =>
+    props.error &&
+    css`
+      border: 2px solid red;
+      &:focus {
+        border: 2px solid red;
+        outline: none;
+      }
+    `}
 `;
 
 export default StyledTextarea;
